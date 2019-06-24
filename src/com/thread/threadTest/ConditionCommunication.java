b0566dce0c4b3093d3608a8ade4c04d3eca29802
yubing <yu46485595@163.com>
2019-06-24 19:35:58 +0800

    111

--- src/com/thread/threadTest/ConditionCommunication.java
+++ src/com/thread/threadTest/ConditionCommunication.java
@@ -12,7 +12,7 @@ import java.util.concurrent.locks.ReentrantLock;
  * 某个实现可以随意移除可能的虚假唤醒，但建议总是假定这些虚假唤醒可能发生,因此总在一个循环中等待.
  *
  * 一个锁内部可以有多个Condition,即有多路等待和通知,可以参考jdk中提供Lock和Condition实现的
- * 可阻赛队列的案例(BoundedBuffer)，在传统的线程机制中国呢一个监视器对象只能有一路等待和通知,
+ * 可阻赛队列的案例(BoundedBuffer)，在传统的线程机制中 一个监视器对象只能有一路等待和通知,
  * 要想实现多路等待和通知,必须嵌套使用多个同步监视器对象.(如果只用一个Condition,两个放的都在等,一旦一个放的进去路,
  * 那么它通知可能会导致另一个放接着往下走)
  *
@@ -51,7 +51,7 @@ public class ConditionCommunication {
                     }
                 }
                 for (int j=1; j<=10; j++) {
-                    System.out.println("子线程第"+i+"次循环,循环第"+j+"次");
+                    System.out.println(Thread.currentThread().getName()+"子线程第"+i+"次循环,循环第"+j+"次");
                 }
                 bShouldSub = false;
                condition.signal();
@@ -72,7 +72,7 @@ public class ConditionCommunication {
                     }
                 }
                 for (int j=1;j<=100;j++){
-                    System.out.println("主线程第" + i + "次循环,循环第" + j + "次");
+                    System.out.println(Thread.currentThread().getName()+"主线程第" + i + "次循环,循环第" + j + "次");
                 }
                 bShouldSub = true;
                 condition.signal();
